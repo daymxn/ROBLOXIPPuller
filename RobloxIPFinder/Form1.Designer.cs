56c6c30b23c4a32d0ef119387dbf58963e4f0971
Daymon <NotDaymon@users.noreply.github.com>
2016-09-22 19:36:13 -0500

    Add files via upload

---
+++ RobloxIPFinder/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RobloxIPFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.simpleButton1.Location = new System.Drawing.Point(52, 31);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(118, 66);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Get Server IP";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(65, 111);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(87, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Made by: Daymon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 136);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ROBLOX IP Puller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
---
+++ RobloxIPFinder/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RobloxIPFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Made by daymon - twitter.com/disband
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+            string appDataFolder = Environment.GetEnvironmentVariable("LocalAppData");
+            string filePath = Path.Combine(appDataFolder, "Roblox\\logs");
+            DirectoryInfo info = new DirectoryInfo(filePath);
+            FileInfo[] files = info.GetFiles().OrderByDescending(p => p.CreationTime).ToArray();
+            foreach (FileInfo fil in files)
+            {
+                if(Convert.ToString(fil).Contains("GameStartScript"))
+                {
+                    filePath = Path.Combine(filePath, Convert.ToString(fil));
+                    break;
+                }   
+            }
+            System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                if(line.Contains("Connecting to"))
+                {
+                    int index = line.IndexOf("to") + 2;
+                    string piece = line.Substring(index);
+                    MessageBox.Show(piece);
+                }
+                counter++;
+            }
+            file.Close();
+        }
+    }
+}
